aa53f35466424685ab45cc436c5b110316fb4846
Adonai Cruz <adonai.cruz@gmail.com>
2017-04-18 22:21:02 -0300

    README

--- src/client/app/in-memory-data.service.ts
+++ src/client/app/in-memory-data.service.ts
@@ -2,7 +2,7 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     let users = [
-      {id: 1, name: 'Adonai',login:'adonai',password:'123',group:'ADMIN'},
+      {id: 1, name: 'Admin',login:'admin',password:'admin',group:'ADMIN'},
       {id: 11, name: 'Mr. Nice',login:'mrnice',password:'123',group:'ADMIN'},
       {id: 12, name: 'Narco',login:'narco',password:'123',group:'USER'},
       {id: 13, name: 'Bombasto',login:'bombasto',password:'123',group:'ADMIN'}
